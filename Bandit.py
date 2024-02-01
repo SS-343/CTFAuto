41638fc3047b9653deeab867653ec86d12ce3cde
SS-343 <60721439+SS-343@users.noreply.github.com>
2024-02-01 01:04:20 -0500

    new

--- Bandit.py
+++ Bandit.py
@@ -33,9 +33,43 @@ def solve_bandit3(pwd):
 	s = ssh(host=banditHost, user='bandit3', password=pwd, port=banditPort)
 	shell = s.run('cd inhere && cat .hidden')
 	nextLevelPass = shell.recvall().decode()
-	print("Bandit3 Password ->", nextLevelPass)
+	print("Bandit4 Password ->", nextLevelPass)
 	s.close()
 	return nextLevelPass
+
+def solve_bandit4(pwd):
+	s = ssh(host=banditHost, user='bandit4', password=pwd, port=banditPort)
+	shell = s.run('cd inhere && find ./ -type f -exec file {} \; | grep ASCII | cut -d: -f1 | xargs cat')
+	nextLevelPass = shell.recvall().decode()
+	print("Bandit5 Password ->", nextLevelPass)
+	s.close()
+	return nextLevelPass
+
+def solve_bandit5(pwd):
+	s = ssh(host=banditHost, user='bandit5', password=pwd, port=banditPort)
+	shell = s.run('cd inhere && find ./ -type f -size 1033c -not -executable | cut -d: -f1 | xargs cat')
+	nextLevelPass = shell.recvall().decode()
+	print("Bandit6 Password ->", nextLevelPass)
+	s.close()
+	return nextLevelPass
+
+def solve_bandit6(pwd):
+        s = ssh(host=banditHost, user='bandit6', password=pwd, port=banditPort)
+        shell = s.run('cd ../ && find / -group bandit6 -user bandit7 2>/dev/null | cut -d: -f1 | xargs cat')
+        nextLevelPass = shell.recvall().decode()
+        print("Bandit7 Password ->", nextLevelPass)
+        s.close()
+        return nextLevelPass
+
+def solve_bandit7(pwd):
+        s = ssh(host=banditHost, user='bandit7', password=pwd, port=banditPort)
+        shell = s.run('cat data.txt | grep millionth | sed \'s/^[ \t]*//;s/[ \t]*$//;s/\bmillionth\b//\'')
+        nextLevelPass = shell.recvall().decode()
+        print("Bandit8 Password ->", nextLevelPass)
+        s.close()
+        return nextLevelPass
+
+
 
 def solve_allBandits():
 	password=["bandit0"] #mutable list to add passwords
@@ -43,5 +77,9 @@ def solve_allBandits():
 	password.append(str(solve_bandit1(password[1]).strip()))
 	password.append(str(solve_bandit2(password[2]).strip()))
 	password.append(str(solve_bandit3(password[3]).strip()))
+	password.append(str(solve_bandit4(password[4]).strip()))
+	password.append(str(solve_bandit5(password[5]).strip()))
+	password.append(str(solve_bandit6(password[6]).strip()))
+	password.append(str(solve_bandit7(password[7]).strip()))
 
 solve_allBandits()
